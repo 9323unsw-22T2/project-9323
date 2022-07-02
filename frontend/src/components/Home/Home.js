deda0fa0493446d3698069c4e96d6d156e24b316
ericphungg1 <80400120+ericphungg1@users.noreply.github.com>
2022-07-03 00:07:33 +1000

    homepage last

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -27,13 +27,13 @@ const Home = () => {
       <Navbar/>
       <View style={{ alignItems: 'flex-end', marginRight: 10, marginTop: 0, float: 'right' }}>
                 <img
-                  style={{ width: 1100, height: 925, marginTop: 0 }}
+                  style={{ width: 1100, height: 935, marginTop: 0 }}
                   src='https://cdn.dribbble.com/users/2217210/screenshots/12205457/media/c2a7d0d3b8d33c298b1e17de263eb7a5.jpg?compress=1&resize=1000x750&vertical=top'
                 />
       </View>
       <Accordians style ={{ position: 'absolute', float: 'left' }}/>
-      <h1 style={{ color: 'darkgray', fontWeight: 'normal', marginLeft: 80, fontSize: 50, fontFamily: 'Roboto' }}> Welcome to Your Online </h1>
-      <h2 style={{ color: 'darkgray', fontWeight: 'normal', marginLeft: 150, fontSize: 50, fontFamily: 'Roboto' }}>Information Hub</h2>
+      <h1 style={{ color: 'darkblue', fontWeight: 'normal', marginLeft: 80, fontSize: 60, fontFamily: 'Roboto' }}> Welcome to Your Online </h1>
+      <h2 style={{ color: 'darkblue', fontWeight: 'normal', marginLeft: 150, fontSize: 60, fontFamily: 'Roboto' }}>Information Hub</h2>
     </div>
 
   );
