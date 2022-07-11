66e81214991352c08f9a01481edbda06f297516f
cartercheng1996 <38425582+cartercheng1996@users.noreply.github.com>
2022-07-11 15:57:56 +1000

    Add profile

--- frontend/src/components/LoggedNavBar/Navbar.jsx
+++ frontend/src/components/LoggedNavBar/Navbar.jsx
@@ -5,14 +5,14 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Fade from '@mui/material/Fade';
 import { styled, alpha } from '@mui/material/styles';
-import SettingsIcon from '@mui/icons-material/Settings';
+// import SettingsIcon from '@mui/icons-material/Settings';
 import InputBase from '@mui/material/InputBase';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import SearchIcon from '@mui/icons-material/Search';
 import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
-import HelpIcon from '@mui/icons-material/Help';
+// import HelpIcon from '@mui/icons-material/Help';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { logOut } from '../../service';
 import { useNavigate } from 'react-router-dom';
@@ -203,17 +203,17 @@ const Navbar = () => {
             <WorkspacePremiumIcon></WorkspacePremiumIcon>
             Expert
           </MenuItem>
-          <MenuItem sx={{ fontSize: '1.2rem' }} onClick={handleClose}>
+          {/* <MenuItem sx={{ fontSize: '1.2rem' }} onClick={handleClose}>
             <SettingsIcon></SettingsIcon>
             Setting
-          </MenuItem>
-          <MenuItem sx={{ fontSize: '1.2rem' }} onClick={(e) => {
+          </MenuItem> */}
+          {/* <MenuItem sx={{ fontSize: '1.2rem' }} onClick={(e) => {
             e.preventDefault()
             navigate('/help')
           }}>
             <HelpIcon></HelpIcon>
             Help
-          </MenuItem>
+          </MenuItem> */}
           <MenuItem sx={{ fontSize: '1.2rem' }} onClick={async (e) => {
             e.preventDefault()
             try {
