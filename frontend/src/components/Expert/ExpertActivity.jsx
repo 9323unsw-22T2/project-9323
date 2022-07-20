f21dce8413c7e598d911cd2b4d1ba7455c7095e5
cartercheng1996 <38425582+cartercheng1996@users.noreply.github.com>
2022-07-20 20:07:51 +1000

    u

--- frontend/src/components/Expert/ExpertActivity.jsx
+++ frontend/src/components/Expert/ExpertActivity.jsx
@@ -54,6 +54,7 @@ export default function VerticalTabs() {
   const [value, setValue] = React.useState(0);
   const sampleData = [
     {
+      id: 1,
       title: 'ESLint with React gives `no-unused-vars` errors',
       photo: ['', '', ''],
       qes: "I've setup eslint & eslint-plugin-react When I run ESLint, the linter returns no-unused-vars errors for each React component. I'm assuming it's not recognizing that I'm using JSX or React syntax. Any ideas?",
@@ -63,6 +64,7 @@ export default function VerticalTabs() {
       time: '2022/02/31 19:49:03'
     },
     {
+      id: 2,
       title: 'Why is java var type variables not working?',
       qes: 'I declared var type variable however the program throws compilation error. Can someone suggest the reason for this error?',
       ans: 'The output from the java -version command you posted above indicates you are using Java 8. var was only introduced in to the language at java 10. If you want to use var you need to install a more recent version of Java - Java 10 or above. This is an include file. It gets literally copy-pasted on your sources. On top of that, the compiler could notify the error around the wrong line, but it could be before or after the one which is actually wrong. Check your sources that include such header, you must miss a semicolon there, or another typo. You should not look into the header. ',
@@ -71,6 +73,7 @@ export default function VerticalTabs() {
       time: '2022/03/11 10:49:03'
     },
     {
+      id: 3,
       title: 'How can I fix this problem with bulding project at Vitis?',
       qes: "I'm trying to build a project with vitis using the library xuartps.h but I can't because of this error code screenchot. I don't know why this happens. Could you help me please?",
       ans: '',
@@ -79,6 +82,7 @@ export default function VerticalTabs() {
       time: '2022/01/11 12:49:03'
     },
     {
+      id: 4,
       title: 'C++ set slower than Java TreeSet?',
       qes: 'I was working on leetcode problem 792. Number of Matching Subsequences, and one of the initial solutions I came up with was to create a list of ordered sets. Then we can determine if a word is a subsequence of string s by trying to find the ceiling of the next available character of string word using the current index we are in of s. If we can reach the end of word, it is then a subsequence, otherwise, it is not.',
       ans: '',
@@ -87,6 +91,7 @@ export default function VerticalTabs() {
       time: '2022/01/11 12:49:03'
     },
     {
+      id: 5,
       title: 'How can I fix this problem with bulding project at Vitis?',
       qes: "I'm trying to build a project with vitis using the library xuartps.h but I can't because of this error code screenchot. I don't know why this happens. Could you help me please?",
       ans: '',
@@ -95,6 +100,9 @@ export default function VerticalTabs() {
       time: '2022/01/11 12:49:03'
     },
   ];
+  if (localStorage.getItem('data') === null) {
+    localStorage.setItem('data', sampleData);
+  }
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
