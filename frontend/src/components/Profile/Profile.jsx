fd34a5518a779b1dc7202d41e8e1b6a9a24145c8
cartercheng1996 <38425582+cartercheng1996@users.noreply.github.com>
2022-08-01 20:17:28 +1000

    Change profile page

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,12 +1,17 @@
+/* eslint-disable no-unused-vars */
+
 import React from 'react';
 import Navbar from '../NavBar/Navbar';
 import LoggedNarbar from '../LoggedNavBar/Navbar';
 import Box from '@mui/material/Box';
-import Photo from '../../profile.jpg';
+import Photo from '../../profile.png';
 import styles from './Profile.module.css';
-import { getInfo } from '../../service'
+import { getInfo, postInfo } from '../../service'
 const App = () => {
   const [data, setData] = React.useState([]);
+  const [error, setError] = React.useState(false);
+  const [userName, setUserName] = React.useState('');
+  const [Password, setPassword] = React.useState('');
   React.useEffect(async () => {
     try {
       const response = await getInfo(localStorage.getItem('token'), localStorage.getItem('user_id'))
@@ -20,43 +25,60 @@ const App = () => {
 
   return (
     <Box sx={{ justifyContent: 'center' }} className={styles.background}>{localStorage.getItem('token') ? (<LoggedNarbar></LoggedNarbar>) : (<Navbar></Navbar>)}
-      <Box sx={{ margin: 'auto', display: 'flex', flexDirection: 'column', mt: 10, width: '100%', height: '60vh', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent', borderWidth: '0px', borderStyle: 'solid' }}>
+      <Box className={styles.card} sx={{ margin: 'auto', display: 'flex', flexDirection: 'column', mt: 10, justifyContent: 'center', alignItems: 'center', borderWidth: '0px', borderStyle: 'solid' }}>
         <div className={styles.title} >My Information</div>
-        <Box sx={{ margin: 'auto', display: 'flex', mt: 10, width: '60%', height: '60vh', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent', borderWidth: '0px', borderStyle: 'solid' }}>
-          <Box sx={{ display: 'flex', flexDirection: 'column', width: '30%', height: '50vh', backgroundColor: 'transparent', borderColor: 'gray', justifyContent: 'flex-start', alignItems: 'center', borderWidth: '0px', borderStyle: 'solid', margin: '5px' }}>
+        <div className={styles.title1}>{data.email}</div>
+        {(error)
+          ? <div className={styles.error} > Username has been used by another user, please try again! </div>
+          : <></>
+        }
+        <Box className={styles.profileLogo2} sx={{ margin: 'auto', display: 'flex', mt: 5, width: '80%', justifyContent: 'center', justifyItems: 'center', alignItems: 'center', alignContent: 'center', backgroundColor: 'transparent', borderWidth: '0px', borderStyle: 'solid' }}>
+          <Box className={styles.profileLogo1} sx={{ flexDirection: 'column', width: '30%', backgroundColor: 'transparent', borderColor: 'gray', justifyContent: 'flex-start', alignContent: 'center', alignItems: 'center', borderWidth: '0px', borderStyle: 'solid', margin: '5px' }}>
             <div className={styles.profileLogo}>
               <img src={Photo} alt="Logo" className={styles.logo} />
+              {/* <button className={styles.button}> Change </button> */}
             </div>
-            <button className={styles.button}> Change </button>
           </Box>
-          <Box sx={{ display: 'flex', flexDirection: 'column', width: '10%', height: '50vh', backgroundColor: 'transparent', justifyContent: 'flex-start', alignItems: 'center', borderColor: 'gray', borderWidth: '0px', borderStyle: 'none', margin: '5px' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', width: '10%', backgroundColor: 'transparent', justifyContent: 'flex-start', alignItems: 'center', borderColor: 'gray', borderWidth: '0px', borderStyle: 'none', margin: '5px' }}>
             <Box sx={{ display: 'flex', flexDirection: 'row', width: '90%', height: '50px', justifyContent: 'flex-start', alignItems: 'center', backgroundColor: 'transparent', borderColor: 'gray', borderWidth: '0px', borderStyle: 'solid', margin: '20px' }}>
               Username:
             </Box>
+
             <Box sx={{ display: 'flex', flexDirection: 'row', width: '90%', height: '50px', justifyContent: 'flex-start', alignItems: 'center', backgroundColor: 'transparent', borderColor: 'gray', borderWidth: '0px', borderStyle: 'solid', margin: '20px' }}>
-              Email:
-            </Box>
-            <Box sx={{ display: 'flex', flexDirection: 'row', width: '90%', height: '50px', justifyContent: 'flex-start', alignItems: 'center', backgroundColor: 'transparent', borderColor: 'gray', borderWidth: '0px', borderStyle: 'solid', margin: '20px' }}>
-              New Password:
+              Password:
             </Box>
             <Box sx={{ display: 'flex', flexDirection: 'row', width: '90%', height: '50px', justifyContent: 'flex-start', alignItems: 'center', backgroundColor: 'transparent', borderColor: 'gray', borderWidth: '0px', borderStyle: 'solid', margin: '20px' }}>
             </Box>
           </Box>
-          <Box sx={{ display: 'flex', flexDirection: 'column', width: '60%', height: '50vh', backgroundColor: 'transparent', justifyContent: 'flex-start', alignItems: 'center', borderColor: 'gray', borderWidth: '0px', borderStyle: 'solid', margin: '5px' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', width: '60%', backgroundColor: 'transparent', justifyContent: 'flex-start', alignItems: 'center', borderColor: 'gray', borderWidth: '0px', borderStyle: 'solid', margin: '5px' }}>
+
             <Box sx={{ display: 'flex', flexDirection: 'row', width: '90%', height: '60px', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent', borderColor: 'gray', borderWidth: '0px', borderStyle: 'solid', margin: '20px' }}>
-              <input className={styles.input} placeholder={data.name} ></input>
+              <input className={styles.input} placeholder={data.name} onChange={() => {
+                setUserName(event.target.value);
+                console.log(event.target.value)
+              }}></input>
             </Box>
             <Box sx={{ display: 'flex', flexDirection: 'row', width: '90%', height: '50px', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent', borderColor: 'gray', borderWidth: '0px', borderStyle: 'solid', margin: '20px' }}>
-              <input className={styles.input} placeholder={data.email} ></input>
-            </Box>
-            <Box sx={{ display: 'flex', flexDirection: 'row', width: '90%', height: '50px', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent', borderColor: 'gray', borderWidth: '0px', borderStyle: 'solid', margin: '20px' }}>
-              <input className={styles.input} placeholder="********"></input>
+              <input className={styles.input} placeholder="********" onChange={() => {
+                setPassword(event.target.value);
+                console.log(event.target.value)
+              }}></input>
             </Box>
             <Box sx={{ display: 'flex', flexDirection: 'row', width: '90%', height: '50px', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent', borderColor: 'gray', borderWidth: '0px', borderStyle: 'solid', margin: '20px' }}>
             </Box>
           </Box>
         </Box>
-        <button className={styles.btn} >Submit</button>
+
+        <button className={styles.btn} onClick={ async () => {
+          try {
+            const res1 = await postInfo({ password: Password.toString(), name: userName.toString() }, localStorage.getItem('token'), localStorage.getItem('user_id'))
+            console.log(res1.data)
+            window.location.reload()
+          } catch (error) {
+            setError(true)
+            console.log(error)
+          }
+        }} > Submit</button>
       </Box>
     </Box>
   );
